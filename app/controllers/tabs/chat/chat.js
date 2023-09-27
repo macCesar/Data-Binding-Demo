286d51984af1faec53286f3e176aa4612e3cccf8
Cesar Estrada <cesar@codigomovil.mx>
2023-09-26 19:22:46 -0500

    code formatting

--- app/controllers/tabs/chat/chat.js
+++ app/controllers/tabs/chat/chat.js
@@ -17,37 +17,37 @@ function scrollChatToBottom(animated = true) {
   $.chatView.scrollToItem(0, Number($.chatView.sections[0].items.length - 1), { animated })
 }
 
-//! Response Simulator
+// ! Response Simulator
 const sender = messages[0].sender
 
-const groupMembers = [...new Set(messages
-  .filter(msg => msg.type === "received")
-  .map(msg => JSON.stringify(msg.sender)))]
+const groupMembers = [ ...new Set(messages
+  .filter(msg => msg.type === 'received')
+  .map(msg => JSON.stringify(msg.sender))) ]
   .map(str => JSON.parse(str))
 
 let someResponses = [
-  "Bye for now.",
-  "Catch you later.",
-  "Goodbye and have a great day.",
-  "Goodbye for now.",
-  "Have a good day/night.",
-  "Have a wonderful day.",
-  "I hope to talk to you again soon.",
-  "I'll be in touch.",
-  "I'm glad we had this chat.",
-  "I've enjoyed our conversation.",
-  "It's been great talking with you.",
-  "See you around.",
-  "See you later.",
-  "Take care and stay safe.",
-  "Take care.",
-  "Take it easy.",
-  "Talk to you soon.",
-  "Thank you for chatting with me.",
-  "Until next time."
+  'Bye for now.',
+  'Catch you later.',
+  'Goodbye and have a great day.',
+  'Goodbye for now.',
+  'Have a good day/night.',
+  'Have a wonderful day.',
+  'I hope to talk to you again soon.',
+  'I\'ll be in touch.',
+  'I\'m glad we had this chat.',
+  'I\'ve enjoyed our conversation.',
+  'It\'s been great talking with you.',
+  'See you around.',
+  'See you later.',
+  'Take care and stay safe.',
+  'Take care.',
+  'Take it easy.',
+  'Talk to you soon.',
+  'Thank you for chatting with me.',
+  'Until next time.'
 ]
 
-function sendMessage(e) {
+function sendMessage() {
   let message = $.message.value.trim()
   if (message) {
     $.message.value = ''
@@ -55,8 +55,8 @@ function sendMessage(e) {
     Alloy.Collections.chat.add(new Backbone.Model({
       sender,
       message,
-      "type": "sent",
-      "timestamp": new Date().toISOString()
+      type: 'sent',
+      timestamp: new Date().toISOString()
     }))
 
     scrollChatToBottom()
@@ -73,9 +73,9 @@ function responseSimulator() {
   _.each(groupMembers, sender => {
     let individualMessage = new Backbone.Model({
       sender,
-      "message": "...",
-      "type": "received",
-      "timestamp": new Date().toISOString()
+      message: '...',
+      type: 'received',
+      timestamp: new Date().toISOString()
     })
 
     setTimeout(() => {
@@ -87,6 +87,6 @@ function responseSimulator() {
         individualMessage.set({ message })
         scrollChatToBottom()
       }, 125 * message.length)
-    }, 1000 * (Math.floor(Math.random() * 6)))
+    }, 1000 * (Math.floor(Math.random() * 8)))
   })
 }
