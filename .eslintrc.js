b613cfb7542a6d5c2323e174d116a9c62dc772d6
Cesar Estrada <cesar@codigomovil.mx>
2023-09-26 19:22:00 -0500

    ESLint setup

---
+++ .eslintrc.js
@@ -0,0 +1,35 @@
+module.exports = {
+  extends: 'axway/env-alloy',
+  globals: {
+    $model: true
+  },
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  rules: {
+    indent: [
+      'error',
+      2
+    ],
+    quotes: [
+      'error',
+      'single'
+    ],
+    semi: [
+      'error',
+      'never'
+    ],
+    'max-statements-per-line': [
+      'error',
+      { max: 2 }
+    ],
+    curly: [
+      'error',
+      'multi'
+    ],
+    'space-before-function-paren': [
+      'error',
+      'never'
+    ]
+  }
+}
